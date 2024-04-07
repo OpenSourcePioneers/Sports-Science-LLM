4860edc3fc48d0e55822dcf0ff5f0881f85f06b3
leon liu <3064617854@qq.com>
2024-04-07 23:19:02 +0800

    update web

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import os
 
-os.system('streamlit run web_internlm2.py --server.address=0.0.0.0 --server.port 7860')
-# os.system('streamlit run web_demo-leon.py --server.address=0.0.0.0 --server.port 7860')
+# os.system('streamlit run web_internlm2.py --server.address=0.0.0.0 --server.port 7860')
+os.system('streamlit run web_demo-leon.py --server.address=0.0.0.0 --server.port 7860')
